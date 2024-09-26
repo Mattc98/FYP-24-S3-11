6f2c07cd8e5d493910ca108497c18b7dfe7d57de
xXerii <ryzlanitegow@gmail.com>
2024-09-27 01:25:44 +0800

    !!DONT MERGE WITH MAIN!! Changes: Updated matts nav bar so the username persists across all pages Can display favourited rooms and room details based on UserID

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,33 @@
-import { calluser } from '@/aws_db/db';
 import LoginForm from "./components/LoginForm/LoginForm";
 import styles from './Home.module.css';
 import Image from 'next/image';
 
 
-interface userAccount{
-  UserID: number;
-  Username: string;
-  Password: string;
-  Email: string;
-  Role: string;
-  BiometricPassword: string;
-  Status: string;
-
-}
-
-async function fetchuser() {
-  try {
-    const response = await calluser("SELECT * FROM userAccount");
-    return JSON.parse(JSON.stringify(response));
-  } catch (error) {
-    console.error(error);
-    throw new Error('Failed to fetch user data.');
-  }
-}
-
 export default async function Home() {
-  const userAccount:userAccount[] = await fetchuser();
+  
 
   return (
-    
     <main className={styles.main}>
-    <div>
-      <div className='text-black'>
-        <h1>This is the home page</h1>
-        <p>Userid: {userAccount[0].UserID}</p>
-        <p>Username: {userAccount[0].Username}</p>
-        <p>password: {userAccount[0].Password}</p>
-    </div>
-   <div className="fixed top-52 left-52">
-      <Image className="object-cover h-[500px] w-[500px]" src="/images/logo.png" alt="Company Logo"  />
-    </div>
-    
-    <div className="fixed top-80 right-20 bg-white p-6 rounded-lg w-full max-w-md">
+      <div>
+
       
-      <h1 className="text-5xl font-bold mb-10 text-center text-black fon">User Login</h1>
+      
+        <div className="fixed top-52 left-52">
+        <Image
+            src="/images/logo.png" // Replace with actual image
+            alt="Company Logo"
+            width={1000}
+            height={1000}
+            className="object-cover h-[500px] w-[500px]"
+          />
+        </div>
     
-      <LoginForm />
-    </div>
-  </div>
-  </main>
+        <div className="fixed top-80 right-20 bg-white p-6 rounded-lg w-full max-w-md">
+          
+          <LoginForm />
+        </div>
+
+      </div>
+    </main>
   );
 }
