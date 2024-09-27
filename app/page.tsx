f5b6c55173fba15501f83f0c30dc0371032d016f
Mattc98 <115862121+Mattc98@users.noreply.github.com>
2024-09-27 14:54:02 +0800

    woopsie fixed app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,46 +4,28 @@ import Image from 'next/image';
 
 
 export default async function Home() {
-  
 
   return (
+    
     <main className={styles.main}>
-    <div>
-      <div className='text-black'>
-        <h1>This is the home page</h1>
-        <p>Userid: {userAccount[0].UserID}</p>
-        <p>Username: {userAccount[0].Username}</p>
-        <p>password: {userAccount[0].Password}</p>
-    </div>
-   <div className="fixed top-52 left-52">
-      <Image
-            src="/images/logo.png" // Replace with actual image
-            alt="Company Logo"
-            width={1000}
-            height={1000}
-            className="object-cover h-[500px] w-[500px]"
-          />
-    </div>
-    
-    <div className="fixed top-80 right-20 bg-white p-6 rounded-lg w-full max-w-md">
-      
-      
+      <div>
         <div className="fixed top-52 left-52">
-        <Image
-            src="/images/logo.png" // Replace with actual image
-            alt="Company Logo"
-            width={1000}
-            height={1000}
-            className="object-cover h-[500px] w-[500px]"
-          />
-        </div>
-    
-        <div className="fixed top-80 right-20 bg-white p-6 rounded-lg w-full max-w-md">
+            <Image
+              src="/images/logo.png" // Replace with actual image
+              alt="Company Logo"
+              width={1000}
+              height={1000}
+              className="object-cover h-[500px] w-[500px]"
+            />
+          </div>
           
-          <LoginForm />
+          <div className="fixed top-80 right-20 bg-white p-6 rounded-lg w-full max-w-md">
+            
+            <h1 className="text-5xl font-bold mb-10 text-center text-black fon">User Login</h1>
+          
+            <LoginForm />
+          </div>
         </div>
-
-      </div>
     </main>
   );
 }
